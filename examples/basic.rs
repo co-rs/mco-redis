9ccde3f3a44303fde1e0840c9433fe68055e8ea4
zhuxiujia@qq.com <zhuxiujia@qq.com>
2022-02-17 20:42:55 +0800

    add connect_timeout

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,8 +1,10 @@
 use mco_redis::cmd;
 use mco_redis::connector::RedisConnector;
 use std::error::Error;
+use std::time::Duration;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // or use connect_timeout(Duration::from_secs(30))
     let redis = RedisConnector::new("127.0.0.1:6379").connect()?;
     redis.exec(cmd::Set("test", "value"))?;
     if let Some(resp) = redis.exec(cmd::Get("test"))? {
--- src/connector.rs
+++ src/connector.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::net::ToSocketAddrs;
+use std::time::Duration;
 use mco::net::TcpStream;
 use crate::bytes::{ByteString};
 use crate::client::Client;
@@ -52,10 +53,41 @@ impl<A> RedisConnector<A>
     where
         A: ToSocketAddrs + Clone,
 {
+    fn _connect_timeout(&mut self, timeout: Duration) -> Result<SimpleClient, ConnectError> {
+        let passwords = self.passwords.clone();
+        let mut addrs = self.address.to_socket_addrs()?;
+        let mut addr = None;
+        loop {
+            match addrs.next() {
+                None => { break; }
+                Some(v) => {
+                    addr = Some(v);
+                }
+            }
+        }
+        if addr.is_none() {
+            return Err(ConnectError::Connect("none socket addr!".to_string()));
+        }
+        let addr = addr.unwrap();
+        let conn = TcpStream::connect_timeout(&addr, timeout.clone())?;
+        conn.set_read_timeout(Some(timeout.clone()));
+        conn.set_write_timeout(Some(timeout));
+        if passwords.is_empty() {
+            Ok(SimpleClient::new(conn))
+        } else {
+            let client = SimpleClient::new(conn);
+            for password in passwords {
+                if client.exec(cmd::Auth(password))? {
+                    return Ok(client);
+                }
+            }
+            Err(ConnectError::Unauthorized)
+        }
+    }
+
     fn _connect(&mut self) -> Result<SimpleClient, ConnectError> {
         let passwords = self.passwords.clone();
         let conn = TcpStream::connect(self.address.clone())?;
-        // io.set_memory_pool(pool);
         if passwords.is_empty() {
             Ok(SimpleClient::new(conn))
         } else {
@@ -74,8 +106,18 @@ impl<A> RedisConnector<A>
         Ok(Client::new(self._connect()?))
     }
 
+    /// Connect to redis server and create shared client with timeout
+    pub fn connect_timeout(&mut self, timeout: Duration) -> Result<Client, ConnectError> {
+        Ok(Client::new(self._connect_timeout(timeout)?))
+    }
+
     /// Connect to redis server and create simple client
     pub fn connect_simple(&mut self) -> Result<SimpleClient, ConnectError> {
         Ok(self._connect()?)
+    }
+
+    /// Connect to redis server and create simple client
+    pub fn connect_simple_timeout(&mut self, timeout: Duration) -> Result<SimpleClient, ConnectError> {
+        Ok(self._connect_timeout(timeout)?)
     }
 }
